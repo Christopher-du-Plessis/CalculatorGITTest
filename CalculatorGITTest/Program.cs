d38f33b6a047baf7c2ab355a26962a3e1c9d6268
Tingerhedgehog <71653747+Tingerhedgehog@users.noreply.github.com>
2020-09-21 18:48:57 +0200

    Update Program.cs
    Added value 1 and 2 variables which the application gets the sum of.

--- CalculatorGITTest/Program.cs
+++ CalculatorGITTest/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +11,18 @@ namespace CalculatorGITTest
         static void Main(string[] args)
         {
             Console.WriteLine("Calculator");
+            Console.WriteLine("Enter value 1");
+            int value2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter value 2");
+            int value1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The calculator will now add value 1 and 2");
+            int Answer = value1 + value2;
+
+            Console.WriteLine(Answer);
+
+            Console.WriteLine("Push any key to end the application . . .");
+            Console.ReadKey();
         }
     }
 }
